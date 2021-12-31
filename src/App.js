698518a72e391684494cf562c41a83f3d7ce1b05
Caleb Wall <48538490+sacredstarfamily@users.noreply.github.com>
2021-12-31 02:07:36 -0800

    add walletconnect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+import React, { useState } from "react";
 import "./styles.css";
 import Moralis from "moralis";
 import { useMoralis } from "react-moralis";
+Moralis.initialize("go0vqtACgpKLrYK5R0AclVjqqFX04NUiTpKpUkj0");
+Moralis.serverURL = "https://y2j9ffhdt6bz.usemoralis.com:2053/server";
+const initialUser = Moralis.User.current();
 export default function App() {
+  const [person, setPerson] = useState(initialUser);
   const { authenticate, isAuthenticated, user, logout } = useMoralis();
-
+  const onloginW = async () => {
+    const person = await Moralis.Web3.authenticate({
+      provider: "walletconnect"
+    });
+    setPerson(person);
+  };
   if (!isAuthenticated) {
     return (
       <div>
         <button
-          onClick={() => authenticate({ signingMessage: "Hello World!" })}
+          onClick={() =>
+            authenticate({
+              provider: "walletconnect",
+              signingMessage: "Hello World!"
+            })
+          }
         >
           Authenticate
         </button>
+        <p>login with walletconnect</p>
+        <button onClick={onloginW}>walletconnect</button>
       </div>
     );
   }
